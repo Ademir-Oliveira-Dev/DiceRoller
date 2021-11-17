595274504ca482c67c8c5a20b2972b1d2f79e346
Ademir Oliveira <ademir.sojunior@gmail.com>
2021-11-17 19:52:14 -0300

    Atividade Dice Roller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonJogar: Button = findViewById(R.id.jogar)
+
+        buttonJogar.setOnClickListener {
+            rollDice()
+        }
+        rollDice()
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        /**
+        val resultadoText: TextView = findViewById(R.id.textView)
+        val resultadoText2: TextView = findViewById(R.id.textView2)
+        resultadoText.text = diceRoll.toString()
+        resultadoText2.text = diceRoll.toString()
+        **/
+        val diceImage: ImageView = findViewById(R.id.img)
+        val drawableResource = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
